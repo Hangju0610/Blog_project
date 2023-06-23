213aa66d883bdeff79466df134ae5b271c8d0b50
hangju0610 <phj950610@naver.com>
2023-06-23 23:11:30 +0900

    posts get, post,put 구현 완료

--- routes/post.js
+++ routes/post.js
@@ -1,36 +1,68 @@
 const express = require('express');
 const router = express.Router();
-// const Posts = require('../schemas/post');
+const Posts = require('../schemas/post');
 
 // 전체 post 목록 가져오기
 router.post('/', async (req, res) => {
     try {
-        const post = {
-            user_id,
+        const createPost = await Posts.create({
+            user_id: req.body.user_id,
             user_password: req.body.user_password,
             post_title: req.body.post_title,
             post_content: req.body.post_content,
-        };
-        console.log('message: 데이터가 완료되었습니다.');
-        res.json(post);
+        });
+        console.log('"message": "데이터가 완료되었습니다."');
+        res.status(201).json(createPost);
     } catch (err) {
-        res.status(400).send('message: ' + err.message);
+        res.status(400).send('message:' + err.message);
     }
 });
 
-// router.get('/', (req, res) => {
-//     res.json(posts);
-// });
+router.get('/', async (req, res) => {
+    try {
+        const postlist = await Posts.find({});
+        const data = { data: postlist };
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(404).send('message:' + err.message);
+    }
+});
 
-// // 하나만 post 목록 가져오기
-// router.get('/:postid', (req, res) => {
-//     const postid = Number(req.params.postid);
-//     const post = posts[postid];
-//     if (post) {
-//         res.json(post);
-//     } else {
-//         res.sendStatus(404);
-//     }
-// });
+router.get('/:postid', async (req, res) => {
+    try {
+        const postid = req.params.postid;
+        const post = await Posts.find({ _id: postid });
+        const data = { data: post };
+
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(404).send('message:' + err.message);
+    }
+});
+
+router.put('/:postid', async (req, res) => {
+    try {
+        const postid = req.params.postid;
+        const body_password = req.body.user_password;
+        const body_title = req.body.post_title;
+        const body_content = req.body.post_content;
+
+        const post = await Posts.find({ _id: postid });
+
+        if (body_password !== post[0]['user_password']) {
+            res.status(400).send('message: 비밀번호가 일치하지 않습니다.');
+        } else if (!body_title || !body_content) {
+            res.status(400).send('message: 데이터 형식이 올바르지 않습니다.');
+        } else {
+            const updatePost = await Posts.updateOne(
+                { _id: postid },
+                { $set: { post_title: body_title, post_content: body_content } }
+            );
+            res.status(200).json(updatePost);
+        }
+    } catch (err) {
+        res.status(404).send('message:' + err.message);
+    }
+});
 
 module.exports = router;
--- schemas/post.js
+++ schemas/post.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const postSchema = new mongoose.Schema({
+    postid: ObjectId,
     user_id: {
         type: String,
         required: true,
@@ -23,23 +25,4 @@ const postSchema = new mongoose.Schema({
     },
 });
 
-// const posts = [
-//     {
-//         post_id: 0,
-//         user_id: 'user',
-//         user_password: '1234',
-//         post_title: 'title',
-//         post_content: 'content',
-//         created_at: '2020-01-01 00:00:00',
-//     },
-//     {
-//         post_id: 1,
-//         user_id: 'user2',
-//         user_password: '12345',
-//         post_title: 'title2',
-//         post_content: 'content2',
-//         created_at: '2020-02-02 00:00:00',
-//     },
-// ];
-
-// module.exports = posts;
+module.exports = mongoose.model('posts', postSchema);

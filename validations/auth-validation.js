5e0a676fd1ec39fbb583a3f073f863b52bf1906f
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-07 18:41:23 +0900

    fix : joi validation 메시지 수정

--- validations/auth-validation.js
+++ validations/auth-validation.js
@@ -2,13 +2,13 @@ const joi = require("joi");
 
 const signupValidation = joi.object({
   nickname: joi.string().not("").min(3).alphanum().required().messages({
-    "any.only": "닉네임의 형식이 일치하지 않습니다.",
+    "any.invalid": "닉네임의 형식이 일치하지 않습니다.",
   }),
   password: joi.string().not("").required().messages({
-    "any.only": "비밀번호를 확인해주세요.",
+    "any.invalid": "비밀번호를 확인해주세요.",
   }),
   confirmPassword: joi.equal(joi.ref("password")).not("").required().messages({
-    "any.only": "비밀번호를 확인해주세요.",
+    "any.invalid": "비밀번호를 확인해주세요.",
   }),
 });
 
